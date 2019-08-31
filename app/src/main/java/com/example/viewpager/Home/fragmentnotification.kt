8ff5d4a62782c409128fad557318620f021dbd3f
robetjanuar <robetjanuar@gmail.com>
2019-08-31 12:55:24 +0700

    project new

---
+++ app/src/main/java/com/example/viewpager/Home/fragmentnotification.kt
@@ -0,0 +1,17 @@
+package com.example.viewpager.Home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewpager.R
+
+class fragmentnotification : Fragment (){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_notification, container, false)
+}
+
